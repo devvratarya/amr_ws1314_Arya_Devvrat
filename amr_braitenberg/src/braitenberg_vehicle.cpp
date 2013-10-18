e574213c6ec2f92196cee49aaae45d6749e29837
devvratarya <devvrat.arya@smail.inf.h-brs.de>
2013-10-18 16:19:01 +0200

    braitenberg_vehicle.cpp with wheelspeedcalculation function

--- amr_braitenberg/src/braitenberg_vehicle.cpp
+++ amr_braitenberg/src/braitenberg_vehicle.cpp
@@ -1,4 +1,11 @@
+//Description : BraitenbergVehicle file for vehicle object creation and speed calculations
+//Modifier : Devvrat Arya
+//Change Descriptio : Added code for speed calculations
+//Modification Date : October 18, 2013
 #include "braitenberg_vehicle.h"
+#include <iostream>
+
+using namespace std;
 
 BraitenbergVehicle::BraitenbergVehicle()
 : type_(TYPE_A)
@@ -22,7 +29,25 @@ void BraitenbergVehicle::computeWheelSpeeds(float left_in, float right_in, float
   //               wheels. Use the parameters stored in the
   //               private fields type_, factor1_, and
   //               factor2_ (if applicable).
-
+	//calculating vehicle speed according the vehicle Type(A,B or C) ad range recieved from sonar
+	Type Veh_type = type_;
+	switch(Veh_type){
+		case TYPE_A:
+				left_out = left_in*factor1_;
+				right_out = right_in*factor1_;
+				break;
+		case TYPE_B:
+				left_out = right_in*factor1_;
+				right_out = left_in*factor1_;
+				break;
+		case TYPE_C:
+				left_out = right_in*factor1_+ left_in*factor2_;
+				right_out = left_in*factor1_ + right_in*factor2_;
+				break;
+		default:
+				std::cout<<"No such braitenberg vehicle found.\n";
+				break;
+	}
 
   // =======================================================
 }
